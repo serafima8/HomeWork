ac34352b8a423d4a21b8159ac6dd7cfbf0a49a24
serafima8 <147659413+serafima8@users.noreply.github.com>
2023-10-31 00:42:13 +0200

    Add files via upload

---
+++ ConsoleApp1(2)/ConsoleApp1(2)/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_2_
+{
+    class Program
+    {
+        //  1. Нахождение максимальной величины из двух целых переменных a, b.
+        //public static int Magnitude(int a,int b)
+        //  {
+
+        //      if(a>b)
+        //      {
+        //          Console.WriteLine($"{a}>{b}");
+        //      }
+        //      else
+        //      {
+        //          Console.WriteLine($"{b}>{a}");
+        //      }
+        //      int max = Math.Max(a,b);
+        //      return max ;
+        //  }
+        //  static void Main(string[] args)
+        //  {
+        //      Console.Write("Введите число a: ");
+        //      int a = int.Parse(Console.ReadLine());
+        //      Console.Write("Введите число b: ");
+        //      int b = int.Parse(Console.ReadLine());
+        //      int Z = Magnitude(a, b);
+        //      Console.Write($"Максимальная величина: {Z}");
+        //      Console.ReadKey();
+        //  }
+
+
+
+        //2. Вычислить среднее арифметическое трех действительных чисел.
+        //public static double ArithmeticMean(double a, double b, double c)
+        //{
+        //    double average = (a + b + c) / 3;
+        //    return average;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Введите первое число: ");
+        //    double a = Convert.ToDouble(Console.ReadLine());
+        //    Console.Write("Введите второе число: ");
+        //    double b = Convert.ToDouble(Console.ReadLine());
+        //    Console.Write("Введите третье число: ");
+        //    double c = Convert.ToDouble(Console.ReadLine());
+        //    double average = ArithmeticMean(a, b, c);
+        //    Console.Write($"Среднее арифметическое: {average}");
+        //    Console.ReadKey();
+        //}
+
+
+       // 3. Составьте таблицу значений функции y = 4x² + 5x - 10 на отрезке[-9; 9] с шагом h = 3.
+        //static void Func()
+        //{
+        //    for (int x = -9; x <= 9; x += 3)
+        //    {
+        //        Console.WriteLine($"x = {x}\t y = {4 * Math.Pow(x, 2) + 5 * x - 10}");
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Func();
+        //    Console.ReadKey();
+        //}
+
+    }
+}
